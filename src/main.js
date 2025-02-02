d32a49c1d6e9c4400879cb74555488e3dd2c945e
lebeanshoe <kevinygx@gmail.com>
2025-02-02 02:23:01 +0100

    ball duplication

--- src/main.js
+++ src/main.js
@@ -23,12 +23,17 @@ const config = {
 const game = new Phaser.Game(config);
 let ball;
 let cursors;
+const entitiesColliderGroup = scene.physics.add.group();
 
 function resizeGame(gameObject) {
     const w = window.innerWidth;
     const h = window.innerHeight;
     game.scale.resize(w, h);
     gameObject.physics.world.setBounds(0, 0, w, h);
+}
+
+function onCollideWithBall() {
+
 }
 
 // Note to self: this part onwards (below this comment) are functions that sits in a Phaser.scene module and is connected via 
@@ -59,11 +64,28 @@ function create() {
 
     // Set ball physics properties
     ball.setBounce(1);
-    ball.setCollideWorldBounds(true);
+    ball.setCollideWorldBounds(true, 1, 1, true);
     ball.setVelocity(Phaser.Math.Between(-200, 200), -300);
 
     // Add keyboard controls
     cursors = this.input.keyboard.createCursorKeys();
+
+    // Add world bound collision event
+    this.physics.world.on('worldbounds', ball => {
+        let newBall = this.physics.add.sprite(400, 300, 'ball');
+
+        newBall.setBounce(1);
+        newBall.setCollideWorldBounds(true, 1, 1, true);
+        newBall.setVelocity(Phaser.Math.Between(-200, 200), -300);
+    });
+
+    this.physics.world.on('collide', (gameObject1, gameObject2) => {
+        console.log('collision');
+        gameObject1.destroy();
+        gameObject2.destroy();
+        // gameObject1.emit('collide', gameObject2);
+        // gameObject2.emit('collide', gameObject1);
+    });
 }
 
 function update() {
