12db01ee5a80073d79929eb7348ec89082cce60c
Ryan Wang <63934683+ryanwanghr@users.noreply.github.com>
2025-02-13 19:15:21 -0800

    changes

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,6 @@ import Phaser from 'phaser';
 import * as simpleSongs from '../songs/simplesongs';
 
 // fix size to window size
-let notes
-let soundCounter = 0
-
 const config: Phaser.Types.Core.GameConfig = {
     type: Phaser.AUTO,
     width: window.innerWidth,
@@ -41,12 +38,6 @@ function onCollideWithBall(b1, b2) {
 
 function onCollideWithWall(ball) {
     console.log("hello")
-    let note = simpleSongs.twinkleTwinkle[soundCounter % simpleSongs.twinkleTwinkle.length]
-    if (note) {
-        notes[note].play();
-    }
-    soundCounter++
-
 
 }
 
@@ -57,20 +48,6 @@ function onCollideWithWall(ball) {
 //     update: update
 // }
 // Part of the config. So "this" refers to shit created in Phaser.scene object that is with Phaser.io library.
-function loadNotes(gameObject: Phaser.Scene) {
-    gameObject.load.audio('C', '../notes/c4.mp3');
-    gameObject.load.audio('C-', '../notes/c-4.mp3');
-    gameObject.load.audio('D', '../notes/d4.mp3');
-    gameObject.load.audio('D-', '../notes/d-4.mp3');
-    gameObject.load.audio('E', '../notes/e4.mp3');
-    gameObject.load.audio('F', '../notes/f4.mp3');
-    gameObject.load.audio('F-', '../notes/f-4.mp3');
-    gameObject.load.audio('G', '../notes/g4.mp3');
-    gameObject.load.audio('G-', '../notes/g-4.mp3');
-    gameObject.load.audio('A', '../notes/a4.mp3');
-    gameObject.load.audio('A-', '../notes/a-4.mp3');
-    gameObject.load.audio('B', '../notes/b4.mp3');
-}
 
 function preload() {
     loadNotes(this)
